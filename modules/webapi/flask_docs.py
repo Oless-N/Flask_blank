baf5e75655bfb573188783b8cef82898f761050f
alx <korklal#gmail.com>
2019-09-10 16:31:59 +0300

    add

---
+++ modules/webapi/flask_docs.py
@@ -0,0 +1,15 @@
+'''ADD keyword for used in html templates'''
+
+'''
+
+
+<!--
+<tr>
+    <td class="column1">1</td>
+    <td class="column2">2017-09-29 01:22</td>
+    <td class="column3">Smith John</td>
+    <td class="column4">$999.00</td>
+    <td class="column5">Malder Fox</td>
+    <td class="column6">Junior worker</td>
+</tr>
+-->'''
